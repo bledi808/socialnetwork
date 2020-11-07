0a03def9306c6ec685c5af892fd15ae39caf7020
Bledi Hasa <bledi.hasa@gmail.com>
2020-11-07 13:32:05 +0100

    conditional rendering Bio

--- src/BioEditor.js
+++ src/BioEditor.js
@@ -6,6 +6,7 @@ export default class BioEditor extends Component {
         super(props);
         this.state = {
             editorIsVisible: false,
+            draftBio: "", // store bio as it is being edited
         };
         // this.toggleEditor = this.toggleEditor.bind(this);
     }
@@ -17,21 +18,59 @@ export default class BioEditor extends Component {
         });
     }
 
-    render() {
-        return (
-            <>
-                <p></p>
+    getCurrentDisplay() {
+        let editor = this.state.editorIsVisible;
+        if (editor) {
+            // edit mode
+            return (
                 <div id="bio-textarea">
-                    {this.state.editorIsVisible && <textarea />}
+                    <textarea />
                 </div>
-                <button
-                    className="button"
-                    id="edit-button"
-                    onClick={() => this.toggleEditor()}
-                >
-                    Toggle Editor
-                </button>
-            </>
+            );
+        } else {
+            return (
+                //display mode
+                <div>
+                    <div>DISPLAY BIO HERE</div>
+                    <button
+                        className="button"
+                        id="edit-button"
+                        onClick={() => this.toggleEditor()}
+                    >
+                        {" "}
+                        Edit Bio
+                    </button>
+                    ;
+                </div>
+            );
+        }
+    }
+
+    handleChange(e) {
+        // console.log("e.target.value", e.target.value);
+        this.setState(
+            {
+                [e.target.name]: e.target.value,
+            }
+            // () => console.log("this.state in the callback: ")
         );
+    }
+
+    render() {
+        return <div>{this.getCurrentDisplay()}</div>; // return (
+        //     <>
+        //         <p></p>
+        //         <div id="bio-textarea">
+        //             {this.state.editorIsVisible && <textarea />}
+        //         </div>
+        //         <button
+        //             className="button"
+        //             id="edit-button"
+        //             onClick={() => this.toggleEditor()}
+        //         >
+        //             Toggle Editor
+        //         </button>
+        //     </>
+        // );
     }
 }
